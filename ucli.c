b808e31a966bfcb8949b2d0f904ff3d00f5ec3ae
Mads Legard Nielsen <90190656+Zetles96@users.noreply.github.com>
2022-09-14 15:59:22 +0200

    this is what we do today

--- ucli.c
+++ ucli.c
@@ -1,10 +1,15 @@
 ///////////////////////////////////////////
 // The University Command Line Interface //
 ///////////////////////////////////////////
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 #include "ucli.h"
 
 int main() {
-    
+    while (true) {
+
+    }
     return 1;
 }
