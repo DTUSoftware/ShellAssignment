3f2295edf8a4858b6d26a1b7fcb6c94a2e2d759e
HeroGamers <hero@herogamers.dev>
2022-09-21 13:38:21 +0200

    Removed build warnings and added cd

--- ucli.c
+++ ucli.c
@@ -1,6 +1,7 @@
 ///////////////////////////////////////////
 // The University Command Line Interface //
 ///////////////////////////////////////////
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -8,16 +9,25 @@
 #include <limits.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "ucli.h"
 
-
 int main() {
+    char cwd[PATH_MAX+1];
     while (true) {
+        fflush(stdout); // Flush if something is stuck :)
+
+        // Getting current working dir
+        if (getcwd(cwd, sizeof(cwd)) == NULL) {
+            strcpy(cwd, "NULL");
+        }
+
         // Reading command-line input
         // We tried using getline(), but it wasn't in MingW, and stumbled upon this implementation that
         // could not really be altered - so credit where credit is due.
         // thanks to https://gist.github.com/btmills/4201660
-        printf("> ");
+        printf("%s > ", cwd);
         fflush(stdout);
 
         unsigned int buffer_size = BUFFERSIZE;
@@ -78,8 +88,8 @@ int main() {
                     else {
                         // this is a very elegant solution
                         // modern problems require modern solutions
-                        strncat(args, " ", 1);
-                        strncat(args, arg, strlen(arg));
+                        strcat(args, " ");
+                        strcat(args, arg);
                     }
                     i++;
                     arg = strtok(NULL, " ");
@@ -91,60 +101,70 @@ int main() {
             // We no longer need to keep the buffer in memory :)
             free(buffer);
 
-            // read list of binaries to find command
-            // https://stackoverflow.com/questions/4204666/how-to-list-files-in-a-directory-in-a-c-program
-            DIR *d;
-            struct dirent *dir;
-            d = opendir("/bin");
-            if (d) {
-                while ((dir = readdir(d)) != NULL) {
-                    // TODO: replace with startswith
-//                    printf("%d\n", strcmp(dir->d_name, command));
-                    if (strstr(dir->d_name, command) != NULL) {
-                        printf("Command found in /bin!\n");
-                        char *old_command = command;
-                        command = (char *) malloc(sizeof(char*) * strlen(command) + sizeof("/bin/"));
-                        strcpy(command, "/bin/");
-                        strncat(command, old_command, strlen(old_command));
-                        free(old_command);
-                        break;
-                    }
-//                    printf("[DEBUG]: %s\n", dir->d_name);
+            // if we want to change working directory
+            if (strcmp("cd", command) == 0) {
+                if (chdir(args) != 0) {
+                    printf("Error changing working directory using %s\n", args);
                 }
-                closedir(d);
-            }
-
-            // Command execution
-            // forking: https://man7.org/linux/man-pages/man2/wait.2.html
-            pid_t cpid, w;
-            int wstatus;
-            cpid = fork();
-            // command the parent should run
-            if (cpid == -1) {
-                perror("fork");
-                exit(EXIT_FAILURE);
-            } else if (cpid == 0) { // child code
-                printf("Command: %s\n", command);
-
-                // call correct binary
-                if (args != NULL) {
-                    printf("Args: %s\n", args);
-                }
-                execl(command, command, args);
-                perror("Child Error");
-                exit(EXIT_FAILURE); // not reached
-            } else { // parent code
                 // free command and args
                 free(command);
                 free(args);
-
-                // https://man7.org/linux/man-pages/man2/wait.2.html
-                do {
-                    w = waitpid(cpid, &wstatus, WUNTRACED | WCONTINUED);
-                    if (w == -1) {
-                        perror("waitpid");
-                        exit(EXIT_FAILURE);
+            }
+            else {
+                // read list of binaries to find command
+                // https://stackoverflow.com/questions/4204666/how-to-list-files-in-a-directory-in-a-c-program
+                DIR *d;
+                struct dirent *dir;
+                d = opendir("/bin");
+                if (d) {
+                    while ((dir = readdir(d)) != NULL) {
+                        // TODO: replace with startswith
+//                    printf("%d\n", strcmp(dir->d_name, command));
+                        if (strstr(dir->d_name, command) != NULL) {
+                            printf("[DEBUG]: Command found in /bin!\n");
+                            char *old_command = command;
+                            command = (char *) malloc(sizeof(char*) * strlen(command) + sizeof("/bin/"));
+                            strcpy(command, "/bin/");
+                            strncat(command, old_command, strlen(old_command));
+                            free(old_command);
+                            break;
+                        }
+//                    printf("[DEBUG]: %s\n", dir->d_name);
                     }
+                    closedir(d);
+                }
+
+                // Command execution
+                // forking: https://man7.org/linux/man-pages/man2/wait.2.html
+                pid_t cpid, w;
+                int wstatus;
+                cpid = fork();
+                // command the parent should run
+                if (cpid == -1) {
+                    perror("fork");
+                    exit(EXIT_FAILURE);
+                } else if (cpid == 0) { // child code
+                    printf("[DEBUG]: Command: %s\n", command);
+
+                    // call correct binary
+                    if (args != NULL) {
+                        printf("[DEBUG]: Args: %s\n", args);
+                    }
+                    execl(command, command, args);
+                    perror("Child Error");
+                    exit(EXIT_FAILURE); // not reached
+                } else { // parent code
+                    // free command and args
+                    free(command);
+                    free(args);
+
+                    // https://man7.org/linux/man-pages/man2/wait.2.html
+                    do {
+                        w = waitpid(cpid, &wstatus, WUNTRACED | WCONTINUED);
+                        if (w == -1) {
+                            perror("waitpid");
+                            exit(EXIT_FAILURE);
+                        }
 
 //                        if (WIFEXITED(wstatus)) {
 //                            printf("exited, status=%d\n", WEXITSTATUS(wstatus));
@@ -155,7 +175,8 @@ int main() {
 //                        } else if (WIFCONTINUED(wstatus)) {
 //                            printf("continued\n");
 //                        }
-                } while (!WIFEXITED(wstatus) && !WIFSIGNALED(wstatus));
+                    } while (!WIFEXITED(wstatus) && !WIFSIGNALED(wstatus));
+                }
             }
         } else {
             free(buffer);
